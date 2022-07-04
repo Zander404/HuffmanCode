6322256a17eb1d411f9868b83869164faccc5ca4
Zander <86967831+Zander404@users.noreply.github.com>
2022-07-04 15:52:38 -0300

    1

---
+++ arvroreBinariaTAD.py
@@ -0,0 +1,10 @@
+def reader(file):
+    with open(file, 'r') as f:
+        return f.read()
+
+
+def writer(file, data):
+    with open(file, 'w') as f:
+        f.write(data)
+
+#leitura de arquivo
---
+++ main.py
@@ -0,0 +1,13 @@
+from Huffman import HuffmanEncoding, HuffmanDecoding
+from arvroreBinariaTAD import reader, writer
+
+
+the_data = reader('test.txt')
+encoding, the_tree = HuffmanEncoding(the_data)
+decoding = HuffmanDecoding(encoding, the_tree)
+print("Arquivo conectado", encoding)
+print("Arquivo descompactado:", decoding)
+print("Tamanho do arquivo:", len(the_data))
+print("Tamanho do arquivo comprimido:", len(encoding))
+writer('test.zin', encoding)
+
